c22dcac8ee00d2cc73e5148da69c9fa5b261d2fb
RamBanjo <rambanjo@gmail.com>
2025-12-12 03:38:33 +0700

    feat: smores quest
    - The quest trades campfire actions for some Max HP.
    - While you have this quest, you can roast marshmallows at campfires for 3 Max HP.
    - If you roast marshmallows twice, you automatically complete this quest and get S'Mores.

---
+++ src/main/java/spireQuests/quests/ramchops/patch/MarshmallowOption.java
@@ -0,0 +1,45 @@
+package spireQuests.quests.ramchops.patch;
+
+import com.badlogic.gdx.assets.loaders.TextureLoader;
+import com.badlogic.gdx.graphics.Color;
+import com.megacrit.cardcrawl.core.CardCrawlGame;
+import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
+import com.megacrit.cardcrawl.helpers.ScreenShake;
+import com.megacrit.cardcrawl.localization.UIStrings;
+import com.megacrit.cardcrawl.ui.campfire.AbstractCampfireOption;
+import com.megacrit.cardcrawl.vfx.BorderFlashEffect;
+import spireQuests.Anniv8Mod;
+import spireQuests.quests.Trigger;
+import spireQuests.quests.ramchops.effects.EatMarshmallowEffect;
+
+import static spireQuests.Anniv8Mod.makeContributionPath;
+import static spireQuests.Anniv8Mod.makeID;
+import static spireQuests.util.TexLoader.getTexture;
+import static spireQuests.util.Wiz.adp;
+
+public class MarshmallowOption extends AbstractCampfireOption {
+    private static final UIStrings uiStrings;
+    public static final String[] TEXT;
+
+    public MarshmallowOption(){
+
+        this.label = TEXT[0];
+        this.description = TEXT[1];
+        this.usable = true;
+
+        this.img = getTexture(makeContributionPath("ramchops", MarshmallowOption.class.getSimpleName()+ ".png"));
+    }
+
+
+
+
+    @Override
+    public void useOption() {
+        AbstractDungeon.effectList.add(new EatMarshmallowEffect());
+    }
+
+    static {
+        uiStrings = CardCrawlGame.languagePack.getUIString(makeID(MarshmallowOption.class.getSimpleName()));
+        TEXT = uiStrings.TEXT;
+    }
+}
