e14f8ae54a716623af1f8f9b80c3fbbe62f0834c
RamBanjo <rambanjo@gmail.com>
2025-12-12 22:00:44 +0700

    chore: refractor marshmallow option

--- src/main/java/spireQuests/quests/ramchops/patch/MarshmallowOption.java
+++ src/main/java/spireQuests/quests/ramchops/patch/MarshmallowOption.java
@@ -18,28 +18,19 @@ import static spireQuests.util.TexLoader.getTexture;
 import static spireQuests.util.Wiz.adp;
 
 public class MarshmallowOption extends AbstractCampfireOption {
-    private static final UIStrings uiStrings;
-    public static final String[] TEXT;
+    private static final UIStrings uiStrings = CardCrawlGame.languagePack.getUIString(makeID(MarshmallowOption.class.getSimpleName()));
+    public static final String[] TEXT = uiStrings.TEXT;
 
     public MarshmallowOption(){
 
         this.label = TEXT[0];
         this.description = TEXT[1];
         this.usable = true;
-
         this.img = getTexture(makeContributionPath("ramchops", MarshmallowOption.class.getSimpleName()+ ".png"));
     }
-
-
-
 
     @Override
     public void useOption() {
         AbstractDungeon.effectList.add(new EatMarshmallowEffect());
-    }
-
-    static {
-        uiStrings = CardCrawlGame.languagePack.getUIString(makeID(MarshmallowOption.class.getSimpleName()));
-        TEXT = uiStrings.TEXT;
     }
 }
